991240f523bb288655db54d168e67864e6098e4d
Kratakey <Kratakey@gmail.com>
2021-07-19 10:00:23 +0300

    add

--- src/test/java/setup/TestBase.java
+++ src/test/java/setup/TestBase.java
@@ -1,15 +1,9 @@
 package setup;
 
-import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import java.util.Locale;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
@@ -19,6 +13,6 @@ public class TestBase {
     public static void setVar() {
         Faker random = new Faker(new Locale("en-US"));
         testVar = random.name().username() + "@dd.dd";
-        System.out.println("Before all:"+testVar);
+        System.out.println("Before all:" + testVar);
     }
 }
