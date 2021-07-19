d42315a1d9bd5a64515ebe7c9d0244402fa2dbfb
Kratakey <Kratakey@gmail.com>
2021-07-19 09:08:25 +0300

    example changed

---
+++ src/test/java/pageObject/TestA.java
@@ -0,0 +1,11 @@
+package pageObject;
+
+import org.junit.jupiter.api.Test;
+
+public class TestA extends setup.TestBase {
+    @Test
+    void a() {
+        System.out.println("A: "+testVar);
+    }
+
+}
---
+++ src/test/java/pageObject/TestB.java
@@ -0,0 +1,10 @@
+package pageObject;
+
+import org.junit.jupiter.api.Test;
+
+public class TestB extends setup.TestBase{
+    @Test
+    void b() {
+        System.out.println("B: "+testVar);
+    }
+}
